fc322a073736c9e61196a6f1ff854a85929c4cbf
AsafSivan1 <asafisivan@gmail.com>
2022-02-27 18:50:00 +0200

    made the embed pretier and made the actual bot run with + the while loop

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 import discord
 from bs4 import BeautifulSoup
 import requests
-import time
 import sys
+from datetime import datetime
+import asyncio
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-token=""
+token = os.getenv('TOKEN')
+
 url="https://www.bbc.com/news/live/world-europe-60542877"
 allowed_stop = [395927930057916434, 494203882973429781, 356492770531344387, 494191985712955394, 513696958087430144]
-
+allowed_eval = [395927930057916434]
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="bbc news"))
-        postTime, title, texts = await self.fetch_new_post()
+        postTime, title, texts= await self.fetch_new_post()
         #first postTime running
 
+        
         with open('last_post.txt', 'r') as f:
             last_post = f.read()
         while True:
-            if last_post != title:
-                #to the loop
-                postTime, title, texts = await self.fetch_new_post()
-                if title.startswith("Breaking"):
-                    embed = discord.Embed(title=title[8:], description="\n".join([x.text for x in texts]) , color=0xff0000)
-                    embed.set_footer(text=postTime+f" UTC, source: {url}")
-                    
-                    await self.get_channel(947236103381983285).send(embed=embed)
+            try:
+                if last_post != title:
+                    #to the loop
+                    postTime, title, texts= await self.fetch_new_post()
+                    if title.startswith("Breaking"):
+                        embed = discord.Embed(title=title[8:], description="\n\n".join([x.text for x in texts]) , color=0xff0000)
+                        embed.timestamp = datetime.now()
+                        embed.set_author(name="bbc news", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/BBC_News_2019.svg/1200px-BBC_News_2019.svg.png", url=url)
+                        await self.get_channel(947236103381983285).send(embed=embed)
+                    else:
+                        embed = discord.Embed(title=title, description="\n\n".join([x.text for x in texts]) , color=0x00ff00)
+                        embed.timestamp = datetime.now()
+                        embed.set_author(name="bbc news", icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/BBC_News_2019.svg/1200px-BBC_News_2019.svg.png", url=url)
+                        await self.get_channel(947236103381983285).send(embed=embed)
+                    last_post = title
+                    with open('last_post.txt', 'w') as f:
+                        f.write(last_post)
                 else:
-                    embed = discord.Embed(title=title, description="\n".join([x.text for x in texts]) , color=0x00ff00)
-                    embed.set_footer(text=postTime+f" UTC, source: {url}")
-                    await self.get_channel(947236103381983285).send(embed=embed)
-                last_post = title
-                with open('last_post.txt', 'w') as f:
-                    f.write(last_post)
-            else:
-                postTime, title, texts = await self.fetch_new_post()
-            time.sleep(60)
-
+                    postTime, title, texts = await self.fetch_new_post()
+                await asyncio.sleep(60)
+            except Exception as e:
+                print(e)
 
         
-        
+    
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -49,6 +58,15 @@ class MyClient(discord.Client):
 
         if message.content == '!stop' and message.author.id in allowed_stop:
             await sys.exit()
+        
+        elif message.content == '!online':
+            await message.channel.send('I am online')
+
+        
+
+        
+            
+        
 
     async def fetch_new_post(self):
         r = requests.get(url)
